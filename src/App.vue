2166c1e0efb5364ab1344e9fc61aacb137b4dcec
leaderli <429243408@qq.com>
2021-09-29 00:33:57 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import errGif from '@/assets/401_images/401.gif'
+
+console.log('------------------>', errGif)
+export default {
+  name: 'App'
+}
+</script>
